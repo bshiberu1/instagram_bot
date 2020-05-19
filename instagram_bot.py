40d87ebb764adcfab23c5feb721915b10369a862
bshiberu1 <bms6@rice.edu>
2020-05-19 15:39:37 -0400

    creating GUI

--- instagram_bot.py
+++ instagram_bot.py
@@ -3,11 +3,37 @@ from selenium import webdriver
 from instapy import InstaPy, smart_run
 
 '''
-This instagram mimics modern instagram bot services by providing services such as 'organically' growing a bot account, 
-and automating a set of bot accounts to like a posts by a series of users 
-'''
+This GUI instagram application mimics modern instagram bot services by providing services such as 'organically' 
+growing a bot account, and automating a set of bot accounts to like a posts by a series of users, must have 
+fireFox installed in order to run this
 
-list_of_proxies = [("8.8.8.8", 8080)]
+To fix errors caused from InstaPy's instagram status checks, comment out the following lines in login_util.py:
+# Hotfix - this check crashes more often than not -- plus in not necessary, I can verify my own connection
+if want_check_browser:
+    if not check_browser(browser, logfolder, logger, proxy_address):
+        return False
+'''
+import sys
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QWidget
+
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QLineEdit
+from PyQt5.QtWidgets import QVBoxLayout
+from PyQt5.QtWidgets import QHBoxLayout
+from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialogButtonBox
+
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QFormLayout
+
+
+from functools import partial
+
+
 list_of_accounts = [("username1", "password1"), ("username2", "password2"), ("username3", "password3")]
 list_of_users = ["username1", "username2", "username3"]
 
@@ -22,7 +48,8 @@ photo_comments = ['Nice shot! @{}',
     ':raised_hands: Yes!',
     'I can feel your passion @{} :muscle:']
 
-def organically_grow_account(session):  
+
+def organically_grow_account(session):
     with smart_run(session):
         session.set_user_interact(amount=3, randomize=True, percentage=100,
                               media='Photo')
@@ -42,24 +69,175 @@ def organically_grow_account(session):
 
         # activity
         session.interact_user_followers([], amount=340)
-        session.join_pods(topic='gaming', engagement_mode='no_comments')
+        session.join_pods(topic='general', engagement_mode='no_comments')
+
 
 def like_user_posts(users, session):
-    # like 50% of posts
-    session.set_do_follow(enabled=False, percentage=50)
-    # put the given comments 80% of the time upon interaction
-    session.set_comments(photo_comments)
-    session.set_do_comment(enabled=True, percentage=80)
-    # like 70% of interacted posts
-    session.set_do_like(True, percentage=70)
-    session.interact_by_users(users, amount=5, randomize=True, media='Photo')
+    with smart_run(session):
+        # like 50% of posts
+        session.set_do_follow(enabled=False, percentage=50)
+        # put the given comments 80% of the time upon interaction
+        session.set_comments(photo_comments)
+        session.set_do_comment(enabled=True, percentage=80)
+        # like 70% of interacted posts
+        session.set_do_like(True, percentage=70)
+        session.interact_by_users(users, amount=5, randomize=True, media='Photo')
 
-session = InstaPy(username=list_of_accounts[0][0], password=list_of_accounts[0][1])
-session.login()
 
-like_user_posts(list_of_users, session)
-organically_grow_account(session)
+class IgBotUi(QMainWindow):
+    """PyCalc's View (GUI)."""
+    def __init__(self):
+        """View initializer."""
+        super().__init__()
+        # Set some main window's properties
+        self.setWindowTitle('Instagram Bot')
+        self.setFixedSize(250, 200)
 
+        # Set the central widget
+        self.generalLayout = QVBoxLayout()
+        self._centralWidget = QWidget(self)        
+        self.setCentralWidget(self._centralWidget)
+
+        self._centralWidget.setLayout(self.generalLayout)
+        self._credentials()
+        self._buttons()
+
+    def _credentials(self):
+        layout = QFormLayout()
+        username = QLineEdit()
+        password = QLineEdit()
+        password.setEchoMode(2)
+        numAccounts = QLineEdit()
+        numAccounts.setMaximumWidth(25)
+        username.setPlaceholderText("username")
+        password.setPlaceholderText("password")
+        self.username = username
+        self.password = password
+        self.numAccounts = numAccounts
+        # self.generalLayout.addWidget(username)
+        layout.addRow('Username:', username)
+        layout.addRow('Password:', password)
+        layout.addRow('# of Accounts:', numAccounts)
+
+        # # Add the display to the general layout
+        self.generalLayout.addLayout(layout)
+
+    '''
+    Launches instagram using the provided log in and automates the account to 
+    like posts and follow other accounts
+    '''
+    def grow(self):
+        session = InstaPy(username=self.username.text(), password=self.password.text())
+        organically_grow_account(session)
+
+    '''
+    Launches the dialog box to get the list of usernames to interact with
+    '''
+    def interact(self):
+        dlg = Dialog(numAccounts = self.numAccounts.text(), view = self)
+        dlg.exec_()
+
+    '''
+    Given the provided usernames will create a new instagram session and 
+    interact with the users on the list
+    '''
+    def interactWithUsers(self, usernames):
+        session = InstaPy(username=self.username.text(), password=self.password.text())
+        like_user_posts(usernames, session)
+
+    '''
+    Initializes the buttons on the main window
+    '''
+    def _buttons(self):
+        self.pushButtons = {}
+
+        self.pushButtons["organicallyGrow"] = QPushButton("Organically Grow Account")
+        self.pushButtons["likeUserPosts"] = QPushButton("Designate Account to interact with Users")
+
+        self.generalLayout.addWidget(self.pushButtons["organicallyGrow"])
+        self.generalLayout.addWidget(self.pushButtons["likeUserPosts"])
+
+
+class Dialog(QDialog):
+    """Dialog."""
+    def __init__(self, parent=None, numAccounts = "3", view=None):
+        """Initializer."""
+        super().__init__(parent)
+        self.view = view
+        self.setWindowTitle('QDialog')
+        dlgLayout = QVBoxLayout()
+        formLayout = QFormLayout()
+
+        # Retreiving the number of accounts interact with
+        try: 
+            numAccount = int(numAccounts)
+        except Exception:
+            numAccount = 3
+        if (numAccount > 20 or numAccount < 1):
+            numAccount = 3
+
+        # Adding all the QlineEdits to a collective array that's used to
+        # amass all the usernames entries
+        self.usernames = []
+        for i in range(numAccount):
+            self.usernames.append(QLineEdit())
+        for username in self.usernames:
+            formLayout.addRow('username:', username)
+
+        # Ok/Cancel Buttons    
+        dlgLayout.addLayout(formLayout)
+        btns = QDialogButtonBox()
+        btns.setStandardButtons(
+            QDialogButtonBox.Cancel | QDialogButtonBox.Ok)
+             
+        # Actions depending on whether OK or Cancel are clicked on
+        btns.accepted.connect(self.addAccounts)
+
+        dlgLayout.addWidget(btns)
+        self.setLayout(dlgLayout)
+
+    def addAccounts(self):
+        usernamesStr = []
+        for username in self.usernames:
+            usernamesStr.append(username.text())
+        self.close()
+        self.view.interactWithUsers(usernamesStr)
+
+
+class IgBotCtrl:
+    """IgBot Controller class."""
+    def __init__(self, view):
+        """Controller initializer."""
+        self._view = view
+        # Connect signals and slots
+        self._connectSignals()
+  
+    def _connectSignals(self):
+        self._view.pushButtons["organicallyGrow"].clicked.connect(self._view.grow)
+        self._view.pushButtons["likeUserPosts"].clicked.connect(self._view.interact)
+        # self._view.display.returnPressed.connect(self._growAccount)
+
+# Client code
+def main():
+    """Main function."""
+    # Create an instance of QApplication
+    igBot = QApplication(sys.argv)
+    
+    # Show the calculator's GUI
+    view = IgBotUi()
+    view.show()
+
+    # Create instances of the model and the controller
+    IgBotCtrl(view=view)
+    # print("controller exected")
+
+    # Execute the calculator's main loop
+    sys.exit(igBot.exec_())
+
+if __name__ == '__main__':
+    main()
+
+'''
 # Initial use of Page Object Patterns to log into instagram (Deprecated)
 class LaunchLogin:
     def __init__(self, browser):
@@ -84,3 +262,5 @@ def test_login_selenium():
     browser.implicitly_wait(5)
     login_page = LaunchLogin(browser)
     login_page.login("username", "password")
+
+'''
